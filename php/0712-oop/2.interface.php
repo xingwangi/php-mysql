912ff06d587487e892b8e870acb5926917e50f95
yunie <2859913655@qq.com>
2019-01-26 20:04:19 +0800

    submit

--- php/0712-oop/2.interface.php
+++ php/0712-oop/2.interface.php
@@ -1,34 +1,57 @@
 <?php
+
 //接口，接口中定义的所有方法都必须是公有（public)，不能是 （protected）和 （private）这是接口的特性
-interface Model{
-	public function get();
-	public function find($id);
-	public function all($id,$var);
+interface Model
+{
+    public function get();
+
+    public function find($id);
+
+    public function all($id, $var);
 
 //    protected function error();  // 错误的
 }
-interface Db{
-	public function connect();
+
+interface Db
+{
+    public function connect();
 }
 
 //可以实现多个接口
-class M implements Model,Db{
-	public function connect() {
-		// TODO: Implement connect() method.
-	}
-    protected  $name;
-	private $fa;
+class M implements Model, Db
+{
+    public function connect()
+    {
+        // TODO: Implement connect() method.
+        echo 'connect';
+
+    }
 
 
-	public function get(){
-		echo 'M 里面的 get';
-	}
-	public function find($id){}
 
-	public function all($id,$var){
+    public function get()
+    {
+        echo 'M 里面的 get';
+    }
 
-	}
+    public function find($id)
+    {
+    }
+
+    public function all($id, $var)
+    {
+
+    }
+    protected $name;
+    private $fa;
+    public function sta()
+    {
+        echo 'sta';
+    }
+
 }
 
 $m = new M;
 $m->get();
+$m->sta();
+$m->connect();
